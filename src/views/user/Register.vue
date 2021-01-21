4ec93bfbef7796f9dd2666542e1ce09d2cdabf71
詹楚江 <chujiang.zhan@dreampix.cn>
2021-01-21 16:40:21 +0800

    register

--- src/views/user/Register.vue
+++ src/views/user/Register.vue
@@ -11,27 +11,16 @@
         ></a-input>
       </a-form-item>
 
-      <a-popover placement="rightTop" trigger="click" :visible="state.passwordLevelChecked">
-        <template slot="content">
-          <div :style="{ width: '240px' }" >
-            <div :class="['user-register', passwordLevelClass]">强度：<span>{{ passwordLevelName }}</span></div>
-            <a-progress :percent="state.percent" :showInfo="false" :strokeColor=" passwordLevelColor " />
-            <div style="margin-top: 10px;">
-              <span>请至少输入 6 个字符。请不要使用容易被猜到的密码。</span>
-            </div>
-          </div>
-        </template>
-        <a-form-item>
-          <a-input
-            size="large"
-            type="password"
-            @click="handlePasswordInputClick"
-            autocomplete="false"
-            placeholder="至少6位密码，区分大小写"
-            v-decorator="['password', {rules: [{ required: true, message: '至少6位密码，区分大小写'}, { validator: this.handlePasswordLevel }], validateTrigger: ['change', 'blur']}]"
-          ></a-input>
-        </a-form-item>
-      </a-popover>
+      <a-form-item>
+        <a-input
+          size="large"
+          type="password"
+          @click="handlePasswordInputClick"
+          autocomplete="false"
+          placeholder="至少6位密码，区分大小写"
+          v-decorator="['password', {rules: [{ required: true, message: '至少6位密码，区分大小写'}]}]"
+        ></a-input>
+      </a-form-item>
 
       <a-form-item>
         <a-input
@@ -42,32 +31,6 @@
           v-decorator="['password2', {rules: [{ required: true, message: '至少6位密码，区分大小写' }, { validator: this.handlePasswordCheck }], validateTrigger: ['change', 'blur']}]"
         ></a-input>
       </a-form-item>
-
-      <!-- <a-form-item>
-        <a-input size="large" placeholder="11 位手机号" v-decorator="['mobile', {rules: [{ required: true, message: '请输入正确的手机号', pattern: /^1[3456789]\d{9}$/ }, { validator: this.handlePhoneCheck } ], validateTrigger: ['change', 'blur'] }]">
-          <a-select slot="addonBefore" size="large" defaultValue="+86">
-            <a-select-option value="+86">+86</a-select-option>
-          </a-select>
-        </a-input>
-      </a-form-item>
-
-      <a-row :gutter="16">
-        <a-col class="gutter-row" :span="16">
-          <a-form-item>
-            <a-input size="large" type="text" placeholder="验证码" v-decorator="['captcha', {rules: [{ required: true, message: '请输入验证码' }], validateTrigger: 'blur'}]">
-              <a-icon slot="prefix" type="mail" :style="{ color: 'rgba(0, 0, 0, .25)' }"/>
-            </a-input>
-          </a-form-item>
-        </a-col>
-        <a-col class="gutter-row" :span="8">
-          <a-button
-            class="getCaptcha"
-            size="large"
-            :disabled="state.smsSendBtn"
-            @click.stop.prevent="getCaptcha"
-            v-text="!state.smsSendBtn && '获取验证码' || (state.time+' s')"></a-button>
-        </a-col>
-      </a-row> -->
 
       <a-form-item>
         <a-button
@@ -88,24 +51,6 @@
 <script>
 import { mixinDevice } from '@/utils/mixin.js'
 
-const levelNames = {
-  0: '低',
-  1: '低',
-  2: '中',
-  3: '强'
-}
-const levelClass = {
-  0: 'error',
-  1: 'error',
-  2: 'warning',
-  3: 'success'
-}
-const levelColor = {
-  0: '#ff0000',
-  1: '#ff0000',
-  2: '#ff7e05',
-  3: '#52c41a'
-}
 export default {
   name: 'Register',
   components: {
@@ -117,56 +62,13 @@ export default {
       state: {
         time: 60,
         smsSendBtn: false,
-        passwordLevel: 0,
-        passwordLevelChecked: false,
         percent: 10,
         progressColor: '#FF0000'
       },
       registerBtn: false
     }
   },
-  computed: {
-    passwordLevelClass () {
-      return levelClass[this.state.passwordLevel]
-    },
-    passwordLevelName () {
-      return levelNames[this.state.passwordLevel]
-    },
-    passwordLevelColor () {
-      return levelColor[this.state.passwordLevel]
-    }
-  },
   methods: {
-
-    handlePasswordLevel (rule, value, callback) {
-      let level = 0
-
-      // 判断这个字符串中有没有数字
-      if (/[0-9]/.test(value)) {
-        level++
-      }
-      // 判断字符串中有没有字母
-      if (/[a-zA-Z]/.test(value)) {
-        level++
-      }
-      // 判断字符串中有没有特殊符号
-      if (/[^0-9a-zA-Z_]/.test(value)) {
-        level++
-      }
-      this.state.passwordLevel = level
-      this.state.percent = level * 30
-      if (level >= 2) {
-        if (level >= 3) {
-          this.state.percent = 100
-        }
-        callback()
-      } else {
-        if (level === 0) {
-          this.state.percent = 10
-        }
-        callback(new Error('密码强度不够'))
-      }
-    },
 
     handlePasswordCheck (rule, value, callback) {
       const password = this.form.getFieldValue('password')
@@ -206,11 +108,11 @@ export default {
               const { msg, code } = res
               if (code === 0) {
                 this.$message.success(msg)
+                $router.push({ name: 'login' })
               } else {
                 this.$message.error(msg)
               }
             })
-          $router.push({ name: 'login' })
           // $router.push({ name: 'login', params: { ...values } })
         }
       })
